ec0e44943732a3141a3108e705fbc05a435d87c2
icodex <icodex.az@gmail.com>
2023-02-10 17:40:16 +0530

    feat: tables export

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import Button from "./Button";
 import PoolCard from "./PoolCard";
 import { StatusWidget, MetricRange, ProgressBar } from "./Widgets";
+import { ApplicationTable, EntityTable, CompanyTable } from "./Table"
 
 
-export { Button, PoolCard, StatusWidget, MetricRange, ProgressBar };
+export { Button, PoolCard, StatusWidget, MetricRange, ProgressBar, ApplicationTable, EntityTable, CompanyTable };
