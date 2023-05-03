6a710645b8bc27db7aef712f8aceb9275873dec6
Aman-zishan <amanzishan.az@gmail.com>
2023-05-04 01:00:45 +0530

    test workflow automation

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,27 @@
-import Button from "./Button";
-import PoolCard from "./PoolCard";
-import { StatusWidget, MetricRange, ProgressBar } from "./Widgets";
-import { ApplicationTable, EntityTable, CompanyTable,SignoffTable, ScreeningTable, PersonsTable, ResearchTable } from "./Table"
+import Button from './Button';
+import PoolCard from './PoolCard';
+import { StatusWidget, MetricRange, ProgressBar } from './Widgets';
+import {
+  ApplicationTable,
+  EntityTable,
+  CompanyTable,
+  SignoffTable,
+  ScreeningTable,
+  PersonsTable,
+  ResearchTable
+} from './Table';
 
-
-export { Button, PoolCard, StatusWidget, MetricRange, ProgressBar,SignoffTable, ApplicationTable, EntityTable, CompanyTable, ScreeningTable, PersonsTable, ResearchTable };
+export {
+  Button,
+  PoolCard,
+  StatusWidget,
+  MetricRange,
+  ProgressBar,
+  SignoffTable,
+  ApplicationTable,
+  EntityTable,
+  CompanyTable,
+  ScreeningTable,
+  PersonsTable,
+  ResearchTable
+};
