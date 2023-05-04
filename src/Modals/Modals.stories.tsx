3dc3910cc68179758b6f7305443963e0e8f68834
Aman-zishan <amanzishan.az@gmail.com>
2023-05-04 09:45:58 +0530

    feat: delete modal

---
+++ src/Modals/Modals.stories.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { action } from '@storybook/addon-actions';
+import { boolean } from '@storybook/addon-knobs';
+import { DeleteModal } from './Modals';
+
+export default {
+  title: 'Modals/delete modal',
+  component: DeleteModal,
+  argTypes: {
+    title: 'string',
+    isOpen: { control: 'boolean' },
+    onClose: { action: 'onClose' }
+  }
+};
+
+const Template = (args) => <DeleteModal {...args}></DeleteModal>;
+
+export const example = Template.bind({});
+example.args = {
+  title: 'delete entity name',
+  isOpen: true,
+  onClose: action('onClose')
+};
